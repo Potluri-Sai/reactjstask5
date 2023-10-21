404b3d33294c58980a47a0ae4eb9bf9521b0942d
potluri sai <aasrithapotluri16@gmail.com>
2023-10-21 04:34:53 -0800

    first commit

---
+++ task5/src/App.js
@@ -0,0 +1,11 @@
+// src/App.js
+import React from 'react';
+import AgeCalculator from './AgeCalculator';
+function App() {
+return (
+<div className="App">
+<AgeCalculator />
+</div>
+);
+}
+export default App;
